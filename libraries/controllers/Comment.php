6c1d254eb1979b414775ccebb28e1e08fa5786bc
Blaster911 <romain588@live.fr>
2022-04-27 19:15:14 +0200

    add autoload.php

--- libraries/controllers/Comment.php
+++ libraries/controllers/Comment.php
@@ -2,12 +2,10 @@
 
 namespace Controllers;
 
-require_once('libraries/models/Comment.php');
-require_once('libraries/models/Article.php');
 require_once('libraries/utils.php');
-require_once('libraries/controllers/Controller.php');
+require_once("libraries/autoload.php");
 
-class Comment
+class Comment extends Controller
 {
     protected $modelName = \Models\Comment::class;
 
