25ca03d8310242d6bd8fd7c4561356beecd62a67
Blaster911 <romain588@live.fr>
2022-04-27 17:46:18 +0200

    add controller

---
+++ libraries/controllers/Article.php
@@ -0,0 +1,30 @@
+<?php
+
+require_once('libraries/utils.php');
+require_once('libraries/models/Article.php');
+
+class Article
+{
+    public function index()
+    {
+        $model = new Article();
+        /**
+         * 2. Récupération des articles
+         */
+        $articles = $model->findAll("created_at DESC");
+
+        /**
+         * 3. Affichage
+         */
+        $pageTitle = "Accueil";
+        render('articles/index', compact('pageTitle', 'articles'));
+    }
+    public function show()
+    {
+        // montrer un article
+    }
+    public function delete()
+    {
+        // supprimer un article
+    }
+}
